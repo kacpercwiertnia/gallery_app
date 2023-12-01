cc1bc14b4c34a9d758d5af48b935a58d7cf24c4e
Mikołaj Pajor <412060@student.agh.edu.pl>
2023-12-01 18:19:06 +0100

    init commit

---
+++ pszczelarze/src/main/java/com/agh/pszczelarze/PszczelarzeApplication.java
@@ -0,0 +1,13 @@
+package com.agh.pszczelarze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PszczelarzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PszczelarzeApplication.class, args);
+	}
+
+}
