8ff01468502af9a70e9d58e456c5d38a41487666
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2023-12-12 19:25:54 +0100

    changes

---
+++ backend/src/main/java/pl/edu/agh/to2/backend/thumbnail/ResizeImageScheduler.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.to2.backend.thumbnail;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class ResizeImageScheduler {
+
+    private ThumbnailService thumbnailService;
+
+    public ResizeImageScheduler(ThumbnailService service){
+        thumbnailService = service;
+    }
+
+    @Scheduled(fixedDelay = 2000L)
+    public void resize(){
+        thumbnailService.test();
+    }
+}
--- backend/src/main/java/pl/edu/agh/to2/backend/thumbnail/ThumbnailService.java
+++ backend/src/main/java/pl/edu/agh/to2/backend/thumbnail/ThumbnailService.java
@@ -1,4 +1,25 @@
 package pl.edu.agh.to2.backend.thumbnail;
 
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import pl.edu.agh.to2.backend.queue.QueueRepository;
+import pl.edu.agh.to2.backend.queue.QueueService;
+
+@Service
 public class ThumbnailService {
+    private ThumbnailRepository thumbnailRepository;
+    private QueueRepository queueRepository;
+
+    public ThumbnailService(ThumbnailRepository repository){
+        thumbnailRepository = repository;
+    }
+
+    @Transactional
+    public void resizeImageFromQueue(){
+
+    }
+
+    public void test(){
+        System.out.println("łysy zamknij dupe");
+    }
 }
