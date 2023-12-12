905619ba21a6691686ff0bf5a4d0da8233b857b8
Kacper Ćwiertnia <411315@student.agh.edu.pl>
2023-12-12 19:27:06 +0100

    Fixed Queue #2

--- backend/src/main/java/pl/edu/agh/to2/backend/queue/Queue.java
+++ backend/src/main/java/pl/edu/agh/to2/backend/queue/Queue.java
@@ -2,22 +2,16 @@ package pl.edu.agh.to2.backend.queue;
 
 import jakarta.persistence.*;
 import pl.edu.agh.to2.backend.image.Image;
-import pl.edu.agh.to2.backend.thumbnail.Thumbnail;
-import pl.edu.agh.to2.backend.thumbnail.ThumbnailSize;
-
-import java.util.List;
 
 @Entity
 public class Queue {
     @Id
     @GeneratedValue
     private int itemId;
-    private ThumbnailSize size;
     @OneToOne
     private Image image;
 
-    public Queue(ThumbnailSize size, Image image){
-        this.size = size;
+    public Queue(Image image){
         this.image = image;
     }
 
@@ -26,10 +20,6 @@ public class Queue {
 
     public int getItemId(){
         return itemId;
-    }
-
-    public ThumbnailSize getSize() {
-        return size;
     }
 
     public Image getImage(){
