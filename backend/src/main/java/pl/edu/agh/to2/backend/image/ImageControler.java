73a16b89057a3176b3127729db483260bc54bef0
Mikołaj Pajor <412060@student.agh.edu.pl>
2023-12-13 19:33:00 +0100

    working rest, filtering thumbnails by size

--- backend/src/main/java/pl/edu/agh/to2/backend/image/ImageControler.java
+++ backend/src/main/java/pl/edu/agh/to2/backend/image/ImageControler.java
@@ -1,25 +1,32 @@
 package pl.edu.agh.to2.backend.image;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.edu.agh.to2.backend.thumbnail.ThumbnailService;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/images")
 public class ImageControler {
     private ImageService imageService;
+    private ThumbnailService thumbnailService;
 
-    public ImageControler(ImageService imageService) {
+    public ImageControler(ImageService imageService, ThumbnailService thumbnailService) {
+        this.thumbnailService = thumbnailService;
         this.imageService = imageService;
     }
 
-    @GetMapping(value = "/get_them")
-    public ResponseEntity getImages() {
+    @GetMapping(value = "/get_them/{size}")
+    public ResponseEntity<GetResponse> getImages(@PathVariable String size) {
         System.out.println("Wysyłam obrazek");
-        String base64Image = imageService.getImages();
-        return ResponseEntity.ok().body(base64Image);
+        List<String> base64Images = thumbnailService.getThumbnailsBySize(size);
+        GetResponse response = new GetResponse(base64Images);
+        return ResponseEntity.ok().body(response);
     }
 
     @PostMapping("/post_them")
--- backend/src/main/java/pl/edu/agh/to2/backend/image/ImageService.java
+++ backend/src/main/java/pl/edu/agh/to2/backend/image/ImageService.java
@@ -6,12 +6,12 @@ import pl.edu.agh.to2.backend.queue.QueueService;
 import java.util.Base64;
 import java.util.List;
 
-import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
 
 @Service
 public class ImageService {
     private final QueueService queueService;
     private final ImageRepository imageRepository;
+
 
     public ImageService(QueueService queueService, ImageRepository imageRepository) {
         this.queueService = queueService;
@@ -26,9 +26,4 @@ public class ImageService {
         queueService.queueImages(image);
     }
 
-    public String getImages() {
-        List<Image> images = imageRepository.findAll();
-        String encoded = Base64.getEncoder().encodeToString(images.get(0).getSource());
-        return encoded;
-    }
 }
