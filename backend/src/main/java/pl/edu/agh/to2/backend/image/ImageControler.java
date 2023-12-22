63154c4edaec9692937fd4d329ecaabc8f77754d
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2023-12-22 13:06:33 +0100

    stylish things

--- backend/src/main/java/pl/edu/agh/to2/backend/image/ImageControler.java
+++ backend/src/main/java/pl/edu/agh/to2/backend/image/ImageControler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.edu.agh.to2.backend.rest.ImagesIdsResponse;
 import pl.edu.agh.to2.backend.rest.SendImagesRequest;
 
 import java.util.List;
@@ -20,19 +21,19 @@ public class ImageControler {
     }
 
     @PostMapping
-    public ResponseEntity sendImages(@RequestBody SendImagesRequest request) {
+    public ResponseEntity<String> sendImages(@RequestBody SendImagesRequest request) {
         try {
             imageService.addNewImages(request.images());
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body("Not a valid encoded image sent for at least one item.");
         }
-        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+        return ResponseEntity.ok().body("Success");
     }
 
     @GetMapping
-    public ResponseEntity<List<Integer>> getImagesIds(){
+    public ResponseEntity<ImagesIdsResponse> getImagesIds(){
         var imagesIds = imageService.getImagesIds();
-        return ResponseEntity.ok(imagesIds);
+        return ResponseEntity.ok(new ImagesIdsResponse(imagesIds));
     }
 }
 
