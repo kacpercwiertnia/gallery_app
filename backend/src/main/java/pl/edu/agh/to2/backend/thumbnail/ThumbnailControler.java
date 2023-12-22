d0d84c53b0ff1bc8cd64476b8ef21bce7dfd50e7
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2023-12-22 12:56:42 +0100

    imports fixed

--- backend/src/main/java/pl/edu/agh/to2/backend/thumbnail/ThumbnailControler.java
+++ backend/src/main/java/pl/edu/agh/to2/backend/thumbnail/ThumbnailControler.java
@@ -3,7 +3,10 @@ package pl.edu.agh.to2.backend.thumbnail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.to2.backend.image.ImageService;
 import pl.edu.agh.to2.backend.rest.ThumbnailsRequest;
 
