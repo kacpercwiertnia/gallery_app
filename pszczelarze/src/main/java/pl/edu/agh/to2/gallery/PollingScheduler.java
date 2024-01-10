162e6b11b0dedf69850da1f9193c058eed2c2e21
Mikołaj Pajor <412060@student.agh.edu.pl>
2024-01-10 19:22:08 +0100

    fix after M2 review

--- pszczelarze/src/main/java/pl/edu/agh/to2/gallery/PollingScheduler.java
+++ pszczelarze/src/main/java/pl/edu/agh/to2/gallery/PollingScheduler.java
@@ -3,21 +3,24 @@ package pl.edu.agh.to2.gallery;
 import javafx.application.Platform;
 import pl.edu.agh.to2.Main;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class PollingScheduler extends Thread{
     private final GalleryControler galleryControler;
+    private final Timer timer = new Timer("Scheduler");
     public PollingScheduler(GalleryControler galleryControler){
         this.galleryControler = galleryControler;
     }
     @Override
     public void run(){
-        try{
-            while(true){
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
                 Main.log.info("Requesting for thumbnails update...");
                 Platform.runLater(galleryControler::refreshThumbnailsLists);
-                Thread.sleep(3000);
             }
-        } catch(InterruptedException exception){
-            Main.log.warning("Exception caught in Scheduler: "+exception.getMessage());
-        }
+        };
+        timer.scheduleAtFixedRate(task, 0,3000);
     }
 }
